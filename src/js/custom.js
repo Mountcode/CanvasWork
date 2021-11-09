69b3ab9121cf26352b3d6209a2c7c39dd6d4709f
Mikhail Ivanov <mikhail.ivanov@best2pay.net>
2021-11-09 10:54:40 +0300

    add anim function

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,18 +1,76 @@
 const canvas = document.querySelector("canvas");
-const CC = canvas.getContext("2d");
-
-function clearCanvas (canvas){
-    canvas.width = canvas.width;
-}
+const context = canvas.getContext("2d");
 
 canvas.width = 500;
 canvas.height = 500;
 
-requestAnimationFrame(tick);
+const triangle = {
+	centerX: canvas.width / 2,
+	centerY: canvas.height / 2,
+	radius: 200,
+	angle: 0,
+	angleSpeed: Math.PI * 0.5,
+};
 
+function animation(params) {
+    const { update, render, clear } = params;
+    let pTimestamp = 0;
 
-function tick(timestamp){
-    requestAnimationFrame(tick)
-    
-    
+    requestAnimationFrame(tick);
+
+    function tick(timestamp) {
+        requestAnimationFrame(tick);
+
+        const diff = timestamp - pTimestamp;
+        const fps = 1000 / diff;
+        const secondPart = diff / 1000;
+        pTimestamp = timestamp;
+
+        const params = {
+            diff,
+            timestamp,
+            pTimestamp,
+            fps,
+            secondPart,
+        };
+
+        update(params);
+        clear();
+        render(params);
+    }
 }
+
+animation({
+	update({ secondPart }) {
+		triangle.angle += triangle.angleSpeed * secondPart;
+	},
+
+	clear() {
+		context.beginPath();
+		context.rect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = "gray";
+		context.fill();
+	},
+
+	render() {
+		const dAngle = (Math.PI / 3) * 4;
+
+		context.beginPath();
+		context.moveTo(
+			triangle.centerX + triangle.radius * Math.cos(triangle.angle),
+			triangle.centerY + triangle.radius * Math.sin(triangle.angle)
+		);
+		context.lineTo(
+			triangle.centerX + triangle.radius * Math.cos(triangle.angle + dAngle),
+			triangle.centerY + triangle.radius * Math.sin(triangle.angle + dAngle)
+		);
+		context.lineTo(
+			triangle.centerX +
+				triangle.radius * Math.cos(triangle.angle + 2 * dAngle),
+			triangle.centerY + triangle.radius * Math.sin(triangle.angle + 2 * dAngle)
+		);
+		context.closePath();
+		context.fillStyle = "red";
+		context.fill();
+	},
+});
